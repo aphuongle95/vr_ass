c2a6c4f98c4c1ce8d24285064a67fa442a924efd
Anh Phuong Le <ciso0478@kronos.medien.uni-weimar.de>
2018-11-16 12:41:38 +0100

    add 3 hinges and arms

--- ass3/lib/Crane.py
+++ ass3/lib/Crane.py
@@ -13,13 +13,13 @@ from lib.Hook import Hook
 
 
 class Crane:
-  
+
     ## constructor
     def __init__(self,
         PARENT_NODE = None,
-        TARGET_LIST = [], # required for bounding box intersection in Hook class 
+        TARGET_LIST = [], # required for bounding box intersection in Hook class
         ):
-        
+
 
         ## init base node for whole crane
         self.base_node = avango.gua.nodes.TransformNode(Name = "base_node")
@@ -29,7 +29,7 @@ class Crane:
 
         ## init internal sub-classes
         self.input = KeyboardInput()
-                     
+
         ## first hinge
         self.hinge0 = Hinge(
             PARENT_NODE = self.base_node,
@@ -38,7 +38,7 @@ class Crane:
             ROT_OFFSET_MAT = avango.gua.make_identity_mat(),
             SF_ROT_INPUT = self.input.sf_rot_input0,
             )
-        
+
         ## first arm segment
         self.arm0 = Arm(
             PARENT_NODE = self.hinge0.hinge_node,
@@ -56,4 +56,32 @@ class Crane:
             SF_ROT_INPUT = self.input.sf_rot_input1,
             )
 
-        
+        ## second arm segment
+        self.arm1 = Arm(
+            PARENT_NODE = self.hinge1.hinge_node,
+            LENGTH = 0.09,
+            DIAMETER = 0.007,
+            ROT_OFFSET_MAT = avango.gua.make_rot_mat(90.0,0,0,1) * avango.gua.make_rot_mat(-45.0,1,0,0),
+            )
+
+        ## third hinge
+        self.hinge2 = Hinge(
+            PARENT_NODE = self.arm1.arm_end_node,
+            DIAMETER = 0.03,
+            HEIGHT = 0.01,
+            ROT_OFFSET_MAT = avango.gua.make_rot_mat(90.0,0,0,1),
+            SF_ROT_INPUT = self.input.sf_rot_input2,
+            )
+
+        ## third arm segment
+        self.arm2 = Arm(
+            PARENT_NODE = self.hinge2.hinge_node,
+            LENGTH = 0.07,
+            DIAMETER = 0.007,
+            ROT_OFFSET_MAT = avango.gua.make_rot_mat(90.0,1,0,0) * avango.gua.make_rot_mat(-50.0,0,0,1),
+            )
+
+        ## hook
+        # self.hook = Hook(
+        #     PARENT_NODE = self.arm2.arm_end_node
+        #     )
